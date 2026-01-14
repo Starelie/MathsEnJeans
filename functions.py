7298542df5d42327718d9f1ce11fd3d3e5c50f93
Starelie <timotheelaberge@gmail.com>
2026-01-14 14:05:56 -0500

    Optimized some of the functions creating lists

--- functions.py
+++ functions.py
@@ -15,29 +15,42 @@ def find_N_Primes(N: int):
         number += 1
     return primes
 
+def find_N_Evens(N: int):
+    return [i * 2 for i in range(N)]
+
 def find_N_Odds(N: int):
-    odds = [0] * N
-    for i in range(len(odds)):
-        odds[i] = i * 2 + 1
-    return odds
+    return [i * 2 + 1 for i in range(N)]
 
 def find_N_Squares(N: int):
-    squares = []
-    for i in range(N):
-        squares.append(i*i)
-    return squares
+    return [i * i for i in range(N)]
 
 def find_N_Powers_Of_X(N: int, X: int):
-    powers = []
-    for i in range(N):
-        powers.append(X**i)
-    return powers
+    return [X ** i for i in range(N)]
 
 def find_N_Values_Fibonacci(N: int):
     fibonacci = [1, 1]
     for i in range(N):
         fibonacci.append(fibonacci[i] + fibonacci[i + 1])
     return fibonacci
+
+def find_N_Conway(N: int):
+    number = "1"
+    conway = [1]
+    for i in range(N):
+        new_number = ""
+        suite = 0
+        current_int = number[0]
+        for j in range(len(number)):
+            if current_int == number[j]:
+                suite += 1
+            else:
+                new_number += str(suite) + current_int
+                current_int = number[j]
+                suite = 1
+        new_number += str(suite) + current_int
+        conway.append(int(new_number))
+        number = new_number
+    return conway
 
 def _print_Line_Spaces(line, spaces, offset):
     print(" " * (spaces // 2) * offset , end="")
@@ -68,17 +81,17 @@ def simple_Print_Triangle(ligneDepart):
     _simple_Print_Line(ligneA)
     for i in range(len(ligneA) - 1):
         ligneB = []
-        for i in range(len(ligneA) - 1):
-            ligneB.append(abs(ligneA[i] - ligneA[i+1]))
+        for j in range(len(ligneA) - 1):
+            ligneB.append(abs(ligneA[j] - ligneA[j+1]))
         _simple_Print_Line(ligneB)
         ligneA = ligneB
 
 def first_Of_Lines_Is_1(ligneDepart):
     ligneA = ligneDepart
-    while (len(ligneA) > 1):
+    for i in range(len(ligneA) - 1):
         ligneB = []
-        for i in range(len(ligneA) - 1):
-            ligneB.append(abs(ligneA[i] - ligneA[i+1]))
+        for j in range(len(ligneA) - 1):
+            ligneB.append(abs(ligneA[j] - ligneA[j+1]))
         if ligneB[0] != 1:
             return False
         ligneA = ligneB
